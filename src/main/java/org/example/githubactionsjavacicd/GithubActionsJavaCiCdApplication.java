923de80d1cafc61d2694a44e63ea9d5b9c7c5cf9
ganislp <ganislp@gmail.com>
2025-03-16 12:47:40 +0200

    testing Cache the Maven packages

--- src/main/java/org/example/githubactionsjavacicd/GithubActionsJavaCiCdApplication.java
+++ src/main/java/org/example/githubactionsjavacicd/GithubActionsJavaCiCdApplication.java
@@ -8,6 +8,7 @@ public class GithubActionsJavaCiCdApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(GithubActionsJavaCiCdApplication.class, args);
+        System.out.println("testing");
     }
 
 }

b0a14a340133cee78aaccefa46a723eba4ca240c
ganislp <ganislp@gmail.com>
2025-03-16 12:10:19 +0200

    init commit

---
+++ src/main/java/org/example/githubactionsjavacicd/GithubActionsJavaCiCdApplication.java
@@ -0,0 +1,13 @@
+package org.example.githubactionsjavacicd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsJavaCiCdApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubActionsJavaCiCdApplication.class, args);
+    }
+
+}

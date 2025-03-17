35e3f61eae05bdc031cf96b2d1598d6ae8cd5b53
ganislp <ganislp@gmail.com>
2025-03-17 05:47:47 +0200

    add upload package

--- src/test/java/org/example/githubactionsjavacicd/GithubActionsJavaCiCdApplicationTests.java
+++ src/test/java/org/example/githubactionsjavacicd/GithubActionsJavaCiCdApplicationTests.java
@@ -3,7 +3,7 @@ package org.example.githubactionsjavacicd;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@RunWith(SpringRunner.class)
+
 @SpringBootTest
 class GithubActionsJavaCiCdApplicationTests {
 
